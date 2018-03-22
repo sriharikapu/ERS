d35a3d4cffae1fe13ff756e208e8f64be09eb05d
sandeep@blockchaintmhub.io <Sep@2017>
2018-03-22 17:22:40 +0800

    icons changed to hr and maps changed to leave management system

---
+++ Dashboard/BS3/assets/js/leave_management.js
@@ -0,0 +1,15 @@
+$('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+  
+})
+    
